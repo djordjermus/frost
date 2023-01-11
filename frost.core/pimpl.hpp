4b86728e0eabf2ebc12ddbeb53fd61d3a362c6bd
Đorđe <djordje.rmus2000@gmail.com>
2023-01-11 22:43:24 +0100

    fix pimpl

--- frost.core/pimpl.hpp
+++ frost.core/pimpl.hpp
@@ -3,25 +3,23 @@
 namespace frost
 {
 	template<class T, u64 index> class pimpl_type;
-	template<class T, u64 index> using pimpl = pimpl_type<T, index>*;
 
 	template<class T, u64 index = 0>
 	class pimpl
 	{
 	public:
-		using pimpl_t = pimpl<T, index>;
 		
 		pimpl<T, index>() :
 			_pimpl() {}
-		pimpl<T, index>(pimpl_t pimpl) :
+		pimpl<T, index>(pimpl_type<T, index>* pimpl) :
 			_pimpl(pimpl) {}
 		pimpl<T, index>(pimpl&& move) noexcept :
 			_pimpl(move.detachPimpl()) {}
 		pimpl<T, index>& operator=(pimpl&& move) noexcept
 		{
-			pimpl_t temp	= swap._pimpl;
-			_pimpl			= swap._pimpl;
-			swap._pimpl		= temp;
+			pimpl_type<T, index>* temp	= move._pimpl;
+			_pimpl						= move._pimpl;
+			move._pimpl					= temp;
 			return *this;
 		}
 
@@ -37,27 +35,27 @@ namespace frost
 		//
 		// METHODS
 
-		pimpl_t getPimpl() const
+		pimpl_type<T, index>* getPimpl() const
 		{
 			return _pimpl;
 		}
 		[[nodiscard]]
-		pimpl_t detachPimpl()
+		pimpl_type<T, index>* detachPimpl()
 		{
-			pimpl_t temp	= _pimpl;
+			pimpl_type<T, index>* temp	= _pimpl;
 			_pimpl			= nullptr;
 			return temp;
 		}
 		[[nodiscard]]
-		pimpl_t swapPimpls(pimpl_t swap)
+		pimpl_type<T, index>* swapPimpls(pimpl_type<T, index>* swap)
 		{
-			pimpl_t temp	= _pimpl;
+			pimpl_type<T, index>* temp	= _pimpl;
 			_pimpl			= swap;
 			return temp;
 		}
 		void swapPimpls(pimpl<T, index>& swap)
 		{
-			pimpl_t temp	= swap._pimpl;
+			pimpl_type<T, index>* temp	= swap._pimpl;
 			_pimpl			= swap._pimpl;
 			swap._pimpl		= temp;
 		}
@@ -71,6 +69,6 @@ namespace frost
 		pimpl<T, index>& operator=(pimpl& copy)					= delete;
 		pimpl<T, index>& operator=(const pimpl&& copy) noexcept = delete;
 	private:
-		pimpl_t _pimpl;
+		pimpl_type<T, index>* _pimpl;
 	};
 }
